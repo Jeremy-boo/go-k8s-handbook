29ea688b537a6d9f5c7eb10dbeb4b7981a962b65
Jeremy-boo <zb_9507@outlook.com>
2020-08-18 17:18:17 +0800

    add client-set

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/go-k8s-handbook
 
 go 1.13
+
+require (
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/apimachinery v0.18.3
+	k8s.io/client-go v0.18.3
+	k8s.io/utils v0.0.0-20200815180417-3bc9d57fc792 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,50 @@
-package go_k8s_handbook
+package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
+)
 
 func main() {
+	kubeConfig := parseK8sConfig()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
+	pods, err := clientSet.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, val := range pods.Items {
+		fmt.Println(val.Name)
+	}
+}
+
+func parseK8sConfig() *string {
+	var k8sConfig *string
+	if home := homeDir(); home != "" {
+		k8sConfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		k8sConfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	return k8sConfig
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
